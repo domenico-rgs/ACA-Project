8167b610be15fa9896f66cc02225379965254824
Domenico Ragusa <domenico.ragusa01@universitadipavia.it>
2021-05-14 19:53:40 +0200

    Fixes some little thing in inversion and mat_gen

--- Serial/mat_inv.c
+++ Serial/mat_inv.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
 
 struct matrix {
 	int ncols;
@@ -12,7 +13,7 @@ struct matrix {
 void readMatrix(struct matrix* m, FILE* file);
 void storeMatrix(struct matrix* m, FILE* file);
 void matrixInversion(struct matrix* m1, struct matrix* m2);
-int computeDeterminant(const struct matrix* m);
+double computeDeterminant(const struct matrix* m);
 
 /* 
 	Reads a matrix from a file and stores it into the appropriate structure.
@@ -44,17 +45,19 @@ void storeMatrix(struct matrix* m, FILE* file) {
 }
 
 /* https://www.codesansar.com/c-programming-examples/matrix-determinant.htm */
-int computeDeterminant(const struct matrix* m) {
+double computeDeterminant(const struct matrix* m) {
 	int i, j, k, ratio;
-	float det = 1;
-	double* mat = (double*)malloc(m->ncols * m->nrows * sizeof(double));
-	mat = memcpy(mat, m->mat, m->nrows * m->ncols * sizeof(double));
+	double det = 1;
+	double* mat;
+	
+	mat = (double*)malloc(m->ncols * m->nrows * sizeof(double));
+	memcpy(mat, m->mat, m->nrows * m->ncols * sizeof(double));
 	
 	/* Using Gauss Elimination technique for transforming matrix to upper triangular matrix */
 	for (i = 0; i < m->nrows; i++) {
 	 	if (mat[i * m->ncols + i] == 0) {
-	 		printf("Mathematical Error: diagonal elements cannot be equal to 0");
-			exit(0);
+			free(mat);
+			return HUGE_VAL;
 		}
 		for (j = i + 1; j < m->nrows; j++) {
 			ratio = mat[j * m->ncols + i] / mat[i * m->ncols + i];
@@ -105,18 +108,30 @@ int main(int argc, char* argv[]) {
 	clock_t t;
 	struct matrix m;
 	float det;
+	
+	/* Files used */
+	mat = fopen(argv[1], "r");
+	resultFile = fopen("inverse.txt", "w");
 
 	/* Reading matrix */
-	mat = fopen(argv[1], "r");
 	fscanf(mat, "%d %d", &m.nrows, &m.ncols);
 	readMatrix(&m, mat);
 
-	/* Checking if it is possible to perform the matrix inversion */
 	det = computeDeterminant(&m);
 	printf("Determinant of the matrix: %f", det);
-	if (det == 0) {
-		printf("\nIt is not possible to compute the inversion: determinant is equal to 0");
+	
+	/* Checking if it is possible to perform the matrix inversion */
+	if(det == HUGE_VAL){
+		printf("Mathematical error in determinant calculation: diagonal elements cannot be equal to 0");
 		fclose(mat);
+		fclose(resultFile);
+		free(m.mat);
+		exit(1);
+	}else if (det == 0 || (m.nrows != m.ncols)) {
+		printf("\nIt is not possible to compute the inversion: determinant is equal to 0 or the number of rows is different thant the cols one");
+		fclose(mat);
+		fclose(resultFile);
+		free(m.mat);
 		exit(1);
 	}
 
@@ -126,13 +141,12 @@ int main(int argc, char* argv[]) {
 	t = clock() - t;
 
 	/* Storing matrix into a file */
-	resultFile = fopen("inverse.txt", "w");
 	storeMatrix(&m, resultFile);
 
 	/* Printing duration in seconds */
 	printf("\nElapsed time: %f seconds", ((double)t) / CLOCKS_PER_SEC);
 
-	/* File closing and Memory cleaning */
+	/* File closing and memory flush */
 	fclose(mat);
 	fclose(resultFile);
 	free(m.mat);

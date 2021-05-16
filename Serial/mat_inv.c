4329105d47d1741553733d0f8be3d13e8928e236
Domenico Ragusa <domenico.ragusa01@universitadipavia.it>
2021-05-16 13:26:55 +0200

    Code presentation

--- Serial/mat_inv.c
+++ Serial/mat_inv.c
@@ -13,9 +13,9 @@ struct matrix {
 void readMatrix(struct matrix* m, FILE* file);
 void storeMatrix(struct matrix* m, FILE* file);
 void matrixInversion(struct matrix* m1, struct matrix* m2);
-double computeDeterminant(const struct matrix* m);
+double computeDeterminant(struct matrix* m);
 
-/* 
+/*
 	Reads a matrix from a file and stores it into the appropriate structure.
 */
 void readMatrix(struct matrix* m, FILE* file) {
@@ -45,34 +45,45 @@ void storeMatrix(struct matrix* m, FILE* file) {
 }
 
 /* https://www.codesansar.com/c-programming-examples/matrix-determinant.htm */
-double computeDeterminant(const struct matrix* m) {
+double computeDeterminant(struct matrix* m) {
 	int i, j, k, ratio;
 	double det = 1;
 	double* mat;
-	
+
 	mat = (double*)malloc(m->ncols * m->nrows * sizeof(double));
-	memcpy(mat, m->mat, m->nrows * m->ncols * sizeof(double));
-	
+	memcpy(mat, m->mat, m->nrows * m->ncols * sizeof(double)); //necessary in order not to change the read matrix into an upper triangular one
+
 	/* Using Gauss Elimination technique for transforming matrix to upper triangular matrix */
 	for (i = 0; i < m->nrows; i++) {
 	 	if (mat[i * m->ncols + i] == 0) {
 			free(mat);
 			return HUGE_VAL;
 		}
-		for (j = i + 1; j < m->nrows; j++) {
+		
+		for (j = i+1 ; j < m->nrows; j++) {
 			ratio = mat[j * m->ncols + i] / mat[i * m->ncols + i];
-	
+
 			for (k = 0; k < m->nrows; k++) {
 				mat[j * m->ncols + k] = mat[j * m->ncols + k] - ratio * mat[i * m->ncols + k];
 			}
 		}
 	}
-	
+
+	printf("\nUpper Triangular Matrix: \n");
+	 for(i=0;i< m->nrows;i++)
+	 {
+		  for(j=0;j< m->nrows;j++)
+		  {
+			   printf("%0.2f\t",mat[i*m->ncols+j]);
+		  }
+		  printf("\n");
+	 }
+
 	/* Finding determinant by multiplying elements in principal diagonal elements */
 	for (i = 0; i < m->nrows; i++) {
         det = det * mat[i * m->ncols + i];
     }
-    
+
     free(mat);
     return det;
 }
@@ -84,7 +95,7 @@ void matrixInversionJacobi(struct matrix* m) {
 	int i, j, k;
 
 	/* Jacobi Algorithm */
-	
+
 }
 
 /*
@@ -97,59 +108,51 @@ void matrixInversionGauss(struct matrix* m) {
 }
 
 int main(int argc, char* argv[]) {
-	/* Checking parameters: 1.mat_inv.exe 2.matrix */
-	if(argc != 2) {
-		printf("Parameters error.");
+	if(argc != 2) { //Checking parameters: 1.mat_inv.exe 2.matrix
+		printf("Parameters error.\n");
 		exit(1);
 	}
 
-	/* Declaring variables */
 	FILE *mat, *resultFile;
 	clock_t t;
 	struct matrix m;
-	float det;
-	
-	/* Files used */
-	mat = fopen(argv[1], "r");
-	resultFile = fopen("inverse.txt", "w");
+	double det;
 
-	/* Reading matrix */
+	mat = fopen(argv[1], "r");
 	fscanf(mat, "%d %d", &m.nrows, &m.ncols);
 	readMatrix(&m, mat);
 
 	det = computeDeterminant(&m);
-	printf("Determinant of the matrix: %f", det);
-	
+
 	/* Checking if it is possible to perform the matrix inversion */
 	if(det == HUGE_VAL){
-		printf("Mathematical error in determinant calculation: diagonal elements cannot be equal to 0");
+		printf("ERROR: Mathematical error in determinant calculation\n");
 		fclose(mat);
 		fclose(resultFile);
 		free(m.mat);
 		exit(1);
 	}else if (det == 0 || (m.nrows != m.ncols)) {
-		printf("\nIt is not possible to compute the inversion: determinant is equal to 0 or the number of rows is different thant the cols one");
+		printf("ERROR: It is not possible to compute the inversion: determinant is equal to 0 or the matrix is not squared\n");
 		fclose(mat);
 		fclose(resultFile);
 		free(m.mat);
 		exit(1);
 	}
 
-	/* Performing inversion and computing its duration*/
+	printf("Determinant of the matrix: %f \n", det);
+
 	t = clock();
 	matrixInversionJacobi(&m);
 	t = clock() - t;
 
-	/* Storing matrix into a file */
+	resultFile = fopen("inverse.txt", "w");
 	storeMatrix(&m, resultFile);
 
-	/* Printing duration in seconds */
-	printf("\nElapsed time: %f seconds", ((double)t) / CLOCKS_PER_SEC);
+	printf("\nElapsed time: %lf seconds\n", ((double)t) / CLOCKS_PER_SEC);
 
-	/* File closing and memory flush */
 	fclose(mat);
 	fclose(resultFile);
 	free(m.mat);
-	
+
 	return 0;
 }

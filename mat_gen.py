3592c4da584dc02b82e9275c9dbc406393556211
Domenico Ragusa <domenico.ragusa01@universitadipavia.it>
2021-05-16 11:56:03 +0200

    Provide more significant output with mat_gen.py

--- mat_gen.py
+++ mat_gen.py
@@ -5,9 +5,15 @@ import numpy as np
 def generate_matrix(nrows, ncols):
     return 2.5*np.random.randn(nrows, ncols)
 
+if len(sys.argv) < 2:
+    print("No argument provided, please provide at least one matrix dimension.\n")
+    sys.exit(1);
+
 for i in range(1,len(sys.argv)):
     nrows, ncols = sys.argv[i].split("x")
     matrix = generate_matrix(int(nrows), int(ncols))
     with open("mat"+str(i)+".txt",'wb') as f:
         f.write((" ".join([nrows, ncols])+"\n").encode())
         np.savetxt(f, matrix, fmt='%.5f')
+
+print("Matrices correctly generated.\n")

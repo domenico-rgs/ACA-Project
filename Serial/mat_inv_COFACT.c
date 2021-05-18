85eb736d90b99685925c2c149bb22749e58cdc28
Domenico Ragusa <domenico.ragusa01@universitadipavia.it>
2021-05-19 00:28:41 +0200

    Add serial version of matrix inversion with LU decomposition

---
+++ Serial/mat_inv_COFACT.c
@@ -0,0 +1,181 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+struct matrix {
+	int ncols;
+	int nrows;
+	double* mat;
+};
+
+void readMatrix(struct matrix* m, FILE* file);
+void storeMatrix(double *squared_matrix, int n, FILE* file);
+double determinant(double *m, int n);
+void getCofactor(double *m, double *cofact, int p, int q, int n);
+void adjoint(struct matrix *m, double *adj);
+void inverse(struct matrix *m, double *inverse, double det);
+
+/*
+Reads a matrix from a file and stores it into the appropriate structure.
+*/
+void readMatrix(struct matrix* m, FILE* file) {
+	int i, j;
+
+	m->mat = (double*)malloc(m->ncols * m->nrows * sizeof(double));
+
+	for (i = 0; i < m->nrows; i++) {
+		for (j = 0; j < m->ncols; j++) {
+			fscanf(file, "%lf", &m->mat[i * m->ncols + j]);
+		}
+	}
+}
+
+/*
+Stores a matrix into the file passed as argument
+*/
+void storeMatrix(double *squared_matrix, int n, FILE* file) {
+	int i, j;
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			fprintf(file, "%lf ", squared_matrix[i * n + j]);
+		}
+		fprintf(file, "\n");
+	}
+}
+
+double determinant(double *m, int n){
+	double d = 0; // Initialize result
+
+	//  Base case : if matrix contains single element
+	if (n == 1)
+	return m[0];
+
+	double *cofact = (double*)malloc(n * n * sizeof(double));  // cofact is used to store cofactors of A[][]
+
+	int sign = 1, f;  // To store sign multiplier
+
+	// Iterate for each element of first row
+	for (f = 0; f < n; f++){
+		// Getting Cofactor of A[0][f]
+		getCofactor(m, cofact, 0, f, n);
+		d += sign * m[f] * determinant(cofact, n - 1);
+
+		// terms are to be added with alternate sign
+		sign = -sign;
+	}
+
+	return d;
+}
+
+void getCofactor(double *m, double *cofact, int p, int q, int n){
+	int i = 0, j = 0, row, col;
+
+	// Looping for each element of the matrix
+	for (row = 0; row < n; row++){
+		for (col = 0; col < n; col++){
+			//  Copying into temporary matrix only those element
+			//  which are not in given row and column
+			if (row != p && col != q){
+				temp[i * n + (j++)] = A[row * n + col];
+
+				// Row is filled, so increase row index and
+				// reset col index
+				if (j == n - 1){
+					j = 0;
+					i++;
+				}
+			}
+		}
+	}
+}
+
+void adjoint(struct matrix *m, double *adj){
+	if (m->nrows == 1)
+	{
+		adj[0] = 1;
+		return;
+	}
+
+	int sign = 1 i, j;
+	double *cofact = (double*)malloc(m->ncols * m->nrows * sizeof(double));  // cofact is used to store cofactors of A[][]
+
+	for (i=0; i<m->nrows; i++)
+	{
+		for (j=0; j<m->ncols; j++)
+		{
+			// Get cofactor of A[i][j]
+			getCofactor(m->mat, cofact, i, j, m->nrows);
+
+			// sign of adj[j][i] positive if sum of row
+			// and column indexes is even.
+			sign = ((i+j)%2==0)? 1: -1;
+
+			// Interchanging rows and columns to get the
+			// transpose of the cofactor matrix
+			adj[j * m->ncols + i] = (sign)*(determinant(cofact, m->nrows-1));
+		}
+	}
+}
+
+void inverse(struct matrix *m, double *inverse, double det){
+	int i, j;
+	// Find adjoint
+	double *adj = (double*)malloc(m->ncols * m->nrows * sizeof(double));
+	adjoint(m, adj);
+
+	// Find Inverse using formula "inverse(A) = adj(A)/det(A)"
+	for (i=0; i<m->nrows; i++){
+		for (j=0; j<m->ncols; j++){
+			inverse[i*m->ncols +j] = adj[i*m->ncols + j]/(double)(det);
+		}
+	}
+}
+
+int main(int argc, char* argv[]) {
+	if(argc != 2) { //Checking parameters: 1.mat_inv.exe 2.matrix
+		printf("Parameters error.\n");
+		exit(1);
+	}
+
+	FILE *mat, *resultFile;
+	clock_t t;
+	struct matrix m;
+	double det;
+
+	mat = fopen(argv[1], "r");
+	fscanf(mat, "%d %d", &m.nrows, &m.ncols);
+	readMatrix(&m, mat);
+
+	det = determinant(&m);
+
+	/* Checking if it is possible to perform the matrix inversion */
+	if (det == 0 || (m.nrows != m.ncols)) {
+		printf("ERROR: It is not possible to compute the inversion: determinant is equal to 0 or the matrix is not squared\n");
+		fclose(mat);
+		fclose(resultFile);
+		free(m.mat);
+		exit(1);
+	}
+
+	printf("Determinant of the matrix: %f \n", det);
+
+	double *inverse =(double*)malloc(m->ncols * m->nrows * sizeof(double));
+
+	t = clock();
+	inverse(&m, inverse, det);
+	t = clock() - t;
+
+	resultFile = fopen("inverse.txt", "w");
+	storeMatrix(inverse, m->nrows, resultFile);
+
+	printf("\nElapsed time: %lf seconds\n", ((double)t) / CLOCKS_PER_SEC);
+
+	fclose(mat);
+	fclose(resultFile);
+	free(m.mat);
+
+	return 0;
+}
---
+++ Serial/mat_inv_LU.c
@@ -0,0 +1,223 @@
+/* https://en.wikipedia.org/wiki/LU_decomposition#C_code_example */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+struct matrix {
+	int ncols;
+	int nrows;
+	double* mat;
+};
+
+void readMatrix(struct matrix* m, FILE* file);
+void storeMatrix(double *squared_matrix, int n, FILE* file);
+double determinant(double *m, int n);
+double LUPDeterminant(double *m, int *P, int n);
+void LUPInvert(double *m, int *P, int n, double *inverseM);
+void LUPSolve(double *m, int *P, double *b, int n, double *x);
+int LUPDecompose(double *m, int n, double Tol, int *P);
+
+/*
+Reads a matrix from a file and stores it into the appropriate structure.
+*/
+void readMatrix(struct matrix* m, FILE* file) {
+	int i, j;
+
+	m->mat = (double*)malloc(m->ncols * m->nrows * sizeof(double));
+
+	for (i = 0; i < m->nrows; i++) {
+		for (j = 0; j < m->ncols; j++) {
+			fscanf(file, "%lf", &m->mat[i * m->ncols + j]);
+		}
+	}
+}
+
+/*
+Stores a matrix into the file passed as argument
+*/
+void storeMatrix(double *squared_matrix, int n, FILE* file) {
+	int i, j;
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			fprintf(file, "%lf ", squared_matrix[i * n + j]);
+		}
+		fprintf(file, "\n");
+	}
+}
+
+/* INPUT: A,P filled in LUPDecompose; N - dimension.
+* OUTPUT: Function returns the determinant of the initial matrix
+*/
+double LUPDeterminant(double *m, int *P, int n){
+	int i;
+	double det = m[0];
+
+	for (i=1; i<n; i++){
+		det *= m[i*n+i];
+	}
+
+	return (P[n] - n) % 2 == 0 ? det : -det;
+}
+
+/* INPUT: A,P filled in LUPDecompose; N - dimension
+* OUTPUT: IA is the inverse of the initial matrix
+*/
+void LUPInvert(double *m, int *P, int n, double *inverseM) {
+	int j, i, k;
+
+	for (j = 0; j < n; j++) {
+		for (i = 0; i < n; i++) {
+			inverseM[i*n+j] = P[i] == j ? 1.0 : 0.0;
+
+			for (k = 0; k < i; k++)
+			inverseM[i*n+j] -= m[i*n+k] * inverseM[k*n+j];
+		}
+
+		for (i = n - 1; i >= 0; i--) {
+			for (k = i + 1; k < n; k++){
+				inverseM[i*n+j] -= m[i*n+k] * inverseM[k*n+j];
+			}
+			inverseM[i*n+j] /= m[i*n+i];
+		}
+	}
+}
+
+/* INPUT: A,P filled in LUPDecompose; b - rhs vector; N - dimension
+ * OUTPUT: x - solution vector of A*x=b
+ */
+ void LUPSolve(double *m, int *P, double *b, int n, double *x) {
+	 int i, k;
+    for (i = 0; i < n; i++) {
+        x[i] = b[P[i]];
+
+        for (k = 0; k < i; k++)
+            x[i] -= m[i*n+k] * x[k];
+    }
+
+    for (i = n - 1; i >= 0; i--) {
+        for (k = i + 1; k < n; k++)
+            x[i] -= m[i*n+k] * x[k];
+
+        x[i] /= m[i*n+i];
+    }
+}
+
+/* INPUT: A - array of pointers to rows of a square matrix having dimension N
+ *        Tol - small tolerance number to detect failure when the matrix is near degenerate
+ * OUTPUT: Matrix A is changed, it contains a copy of both matrices L-E and U as A=(L-E)+U such that P*A=L*U.
+ *        The permutation matrix is not stored as a matrix, but in an integer vector P of size N+1
+ *        containing column indexes where the permutation matrix has "1". The last element P[N]=S+N,
+ *        where S is the number of row exchanges needed for determinant computation, det(P)=(-1)^S
+ */
+int LUPDecompose(double *m, int n, double Tol, int *P) {
+
+    int i, j, k, imax;
+    double maxA, absA, temp;
+
+    for (i = 0; i <= n; i++)
+        P[i] = i; //Unit permutation matrix, P[N] initialized with N
+
+    for (i = 0; i < n; i++) {
+        maxA = 0.0;
+        imax = i;
+
+        for (k = i; k < n; k++)
+            if ((absA = fabs(m[k*n+i])) > maxA) {
+                maxA = absA;
+                imax = k;
+            }
+
+        if (maxA < Tol) return 0; //failure, matrix is degenerate
+
+        if (imax != i) {
+            //pivoting P
+            j = P[i];
+            P[i] = P[imax];
+            P[imax] = j;
+
+            //pivoting rows of A
+						for(j = 0; j < n; j++){
+							temp = m[i*n+j];
+							m[i*n + j] = m[imax*n+j];
+							m[imax*n+j]=temp;
+						}
+
+            //counting pivots starting from n (for determinant)
+            P[n]++;
+        }
+
+        for (j = i + 1; j < n; j++) {
+            m[j*n+i] /= m[i*n+i];
+
+            for (k = i + 1; k < n; k++)
+                m[j*n+k] -= m[j*n+i] * m[i*n+k];
+        }
+    }
+
+    return 1;  //decomposition done
+}
+
+int main(int argc, char* argv[]) {
+	if(argc != 2) { //Checking parameters: 1.mat_inv.exe 2.matrix
+		printf("Parameters error.\n");
+		exit(1);
+	}
+
+	FILE *mat, *resultFile;
+	clock_t t;
+	struct matrix m;
+	double det;
+
+	mat = fopen(argv[1], "r");
+	fscanf(mat, "%d %d", &m.nrows, &m.ncols);
+	readMatrix(&m, mat);
+
+	/*
+	det = determinant(&m);
+
+	if (det == 0 || (m.nrows != m.ncols)) {
+		printf("ERROR: It is not possible to compute the inversion: determinant is equal to 0 or the matrix is not squared\n");
+		fclose(mat);
+		fclose(resultFile);
+		free(m.mat);
+		exit(1);
+	}
+
+	printf("Determinant of the matrix: %f \n", det);
+	*/
+
+	int *P = 	(int*)malloc(m.nrows * sizeof(int) + 1 * sizeof(int));
+	double *inverseM = (double*)malloc(m.ncols * m.nrows * sizeof(double));
+	double *x = (double*)malloc(m.nrows * sizeof(double));
+	double *b = (double*)malloc(m.nrows * sizeof(double));
+
+	t = clock();
+
+	LUPDecompose(m.mat, m.nrows, 1E-3, P);
+	LUPSolve(m.mat, P, b, m.nrows, x);
+	LUPInvert(m.mat, P, m.nrows, inverseM);
+
+	t = clock() - t;
+
+	printf("\nDeterminant: %lf\n", LUPDeterminant(m.mat, P, m.nrows));
+
+	resultFile = fopen("inverse.txt", "w");
+	storeMatrix(inverseM, m.nrows, resultFile);
+
+	printf("\nElapsed time: %lf seconds\n", ((double)t) / CLOCKS_PER_SEC);
+
+	fclose(mat);
+	fclose(resultFile);
+
+	free(P);
+	free(inverseM);
+	free(x);
+	free(b);
+	free(m.mat);
+
+	return 0;
+}

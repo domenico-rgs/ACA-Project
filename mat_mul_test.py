cb65cf65e4440f3fbc91ec5f0bc63741fc92bfe6
danilomodica <danilo.modica01@universitadipavia.it>
2021-05-19 16:06:36 +0200

    Remove matrix outputs + minor fixes

--- mat_mul_test.py
+++ mat_mul_test.py
@@ -12,7 +12,7 @@ def read_matrix(filename):
 
 def store_matrix(mat):
     result = open('result.txt', 'w')
-    result.write(str(len(mat_result)) + " " + str(len(mat_result[0])) + "\n")
+    result.write(str(len(mat)) + " " + str(len(mat[0])) + "\n")
     for el in mat:
         for number in el:
             result.write(str(number) + " ")
@@ -36,23 +36,11 @@ if (rows_cols1[1] != rows_cols2[0]):
     print("It is not possible to do matrix multiplication. Check matrix number of rows and cols.")
     sys.exit(1);
 
-# Matrix 1
-print("\nMatrix 1 is " + str(int(rows_cols1[0])) + "x" + str(int(rows_cols1[1])) + ":")
-for el in mat1:
-    print(el)
-# Matrix 2
-print("\nMatrix 2 is " + str(int(rows_cols2[0])) + "x" + str(int(rows_cols2[1])) + ":")
-for el in mat2:
-    print(el)
-
 # Performing multiplication
 mat_result = [[sum(m1 * m2 for m1, m2 in zip(mat1_row, mat2_col)) 
                         for mat2_col in zip(*mat2)]
                                 for mat1_row in mat1]
 
-# Output of the result
-print("\nResult " + str(len(mat_result)) + "x" + str(len(mat_result[0])) + ":")
-for el in mat_result:
-    print(el)
 # Storing result matrix into a file
 store_matrix(mat_result)
+print("The result matrix has been stored into result.txt")

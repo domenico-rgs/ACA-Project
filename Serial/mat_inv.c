8e6fa26ec890097d9ebd4a3766b3029af102fed7
Domenico Ragusa <domenico.ragusa01@universitadipavia.it>
2021-05-23 18:46:36 +0200

    Matrix linearization

--- Serial/mat_inv.c
+++ Serial/mat_inv.c
@@ -12,11 +12,11 @@ struct matrix {
 void readMatrix(struct matrix* m, FILE* file);
 void storeMatrix(double *squared_matrix, int n, FILE* file);
 double findDeterminant(double *a, int *p, int n);
-void forwardSubstitution(double **l, double **p, double *y, int column, int n);
-void backwardSubstitution(double **u, double *y, double **a_inv, int column, int n);
-void pivoting(double **a, double **p, int n);
-void lu(double **a, double **l, double **u, int n);
-void computeInverse(double *a, double **a_inv, int n);
+void forwardSubstitution(double *l, double *p, double *y, int column, int n);
+void backwardSubstitution(double *u, double *y, double *a_inv, int column, int n);
+void pivoting(double *a, double *p, int n);
+void lu(double *l, double *u, int n);
+void computeInverse(double *a, double *a_inv, int n);
 
 /*
  *	Reads a matrix from a file and stores it into the appropriate structure.
@@ -59,35 +59,35 @@ double findDeterminant(double *a, int *p, int n) {
 	return (p[n] - n) % 2 == 0 ? det : -det;
 }
 
-void forwardSubstitution(double **l, double **p, double *y, int column, int n) {
+void forwardSubstitution(double *l, double *p, double *y, int column, int n) {
 	int i, j;
 	double sum = 0;
 	
     for (i = 0; i < n; i++) {
         for (j = 0; j < i; j++) {
-            sum = sum + l[i][j] * y[j];
+            sum = sum + l[i * n + j] * y[j];
         }
-        y[i] = (p[i][column] - sum) / l[i][i];
+        y[i] = (p[i * n + column] - sum) / l[i * n + i];
         sum = 0;
     }
 }
 
-void backwardSubstitution(double **u, double *y, double **a_inv, int column, int n) {
+void backwardSubstitution(double *u, double *y, double *a_inv, int column, int n) {
     int i, j;
 	double sum;
     
-    a_inv[n-1][column] = y[n-1] / u[n-1][n-1];
+    a_inv[(n-1)*n+column] = y[n-1] / u[(n-1) * n + (n-1)];
     for (i = n - 2; i >= 0; i--) {
 		sum = y[i];
         for (j = n - 1; j > i; j--) {
-           sum = sum - u[i][j] * a_inv[j][column];
+           sum = sum - u[i * n + j] * a_inv[j*n+column];
         }
-        a_inv[i][column] = sum / u[i][i]; 
+        a_inv[i*n+column] = sum / u[i * n + i]; 
         sum = 0;
     }
 }
 
-void pivoting(double **a, double **p, int n) { 
+void pivoting(double *a, double *p, int n) { 
     int j, k;
 	int isMaximum = 0; 
     double *temp = (double*)malloc(n * sizeof(double));	// to perform memory swappings
@@ -97,21 +97,21 @@ void pivoting(double **a, double **p, int n) {
     	int imax = k;
         for (j = k; j < n; j++) { 	
             // finding the maximum index
-			if (a[j][k] > a[imax][k]) { 
+			if (a[j * n + k] > a[imax * n + k]) { 
 				imax = j;
                 isMaximum = 1;
             }
         }
         if (isMaximum == 1) {
         	// swapping a[k] and a[imax]
-			memcpy(temp, a[k], n * sizeof(double));
-			memcpy(a[k], a[imax], n * sizeof(double));
-			memcpy(a[imax], temp, n * sizeof(double));
+			memcpy(temp, &a[k*n], n * sizeof(double));
+			memcpy(&a[k*n], &a[imax*n], n * sizeof(double));
+			memcpy(&a[imax*n], temp, n * sizeof(double));
 			
 			// swapping p[k] and p[imax]
-			memcpy(temp, p[k], n * sizeof(double));
-			memcpy(p[k], p[imax], n * sizeof(double));
-			memcpy(p[imax], temp, n * sizeof(double));
+			memcpy(temp, &p[k*n], n * sizeof(double));
+			memcpy(&p[k*n], &p[imax*n], n * sizeof(double));
+			memcpy(&p[imax*n], temp, n * sizeof(double));
 			
         	isMaximum = 0;
     	}
@@ -119,53 +119,47 @@ void pivoting(double **a, double **p, int n) {
 	free(temp);
 }
 
-void lu(double **a, double **l, double **u, int n) {
+void lu(double *l, double *u, int n) {
     int i, j, k;
     
 	for (k = 0; k < n; k++) {
         for (i = k + 1; i < n; i++) {
-            l[i][k] = u[i][k] / u[k][k];
+            l[i * n + k] = u[i * n + k] / u[k * n + k];
             for (j = k; j < n; j++) {
-                u[i][j] = u[i][j] - l[i][k] * u[k][j];
+                u[i * n + j] = u[i * n + j] - l[i * n + k] * u[k * n + j];
             }
         }
     }
 }
 
-void computeInverse(double *a, double **a_inv, int n) {
+void computeInverse(double *a, double *a_inv, int n) {
     int i, j;
     
     /* Memory allocation of temporary matrix */
-    double **p = (double**)malloc(n * sizeof(double*));
-	double **l = (double**)malloc(n * sizeof(double*));
-	double **a_p = (double**)malloc(n * sizeof(double*));
-	double **u = (double**)malloc(n * sizeof(double*));
+    double *p = (double*)malloc(n * n * sizeof(double));
+	double *l = (double*)malloc(n * n * sizeof(double));
+	double *a_p = (double*)malloc(n * n * sizeof(double));
+	double *u = (double*)malloc(n * n * sizeof(double));
 	double *y = (double*)malloc(n * sizeof(double));
-	for (i = 0; i < n; i++) {
-        l[i] = (double *)malloc(n * sizeof(double));
-        u[i] = (double *)malloc(n * sizeof(double));
-        p[i] = (double *)malloc(n * sizeof(double));
-        a_p[i] = (double *)malloc(n * sizeof(double));
-    }
     
 	/* Creating pivoting matrix and setting inverse matrix to 0 */
 	for (i = 0; i < n; i++) {
-        p[i][i] = 1;
+        p[i * n + i] = 1;
         for (j = 0; j < n; j++) {
-            a_inv[i][j] = 0;
+            a_inv[i*n+j] = 0;
             if (i != j) {
-                p[i][j] = 0;
+                p[i * n + j] = 0;
             }
         }
     }
     
     /* Creating matrix l and copying a into a_p */
     for (i = 0; i < n; i++) {
-        l[i][i] = 1;
+        l[i * n + i] = 1;
         for (j = 0; j < n; j++) {
-            a_p[i][j] = a[i*n + j];
+            a_p[i * n + j] = a[i*n + j];
             if (i != j)
-                l[i][j] = 0;
+                l[i * n + j] = 0;
         }
     }
     
@@ -174,12 +168,12 @@ void computeInverse(double *a, double **a_inv, int n) {
 	/* Creating matrix u using a_p */
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
-            u[i][j] = a_p[i][j];
+            u[i * n + j] = a_p[i * n + j];
         }
     }
 
 	/* Performing LU decomposition */
-    lu(a_p, l, u, n);
+    lu(l, u, n);
     
     /* Finding the inverse, result is stored into a_inv */
 	for (i = 0; i < n; i++) {
@@ -187,10 +181,10 @@ void computeInverse(double *a, double **a_inv, int n) {
         backwardSubstitution(u, y, a_inv, i, n);		// a_inv is filled
     }
     
-    free(*p);		
-	free(*l);
-	free(*u);
-	free(*a_p);
+    free(p);		
+	free(l);
+	free(u);
+	free(a_p);
 	free(y);
 }
 
@@ -204,7 +198,7 @@ int main(int argc, char* argv[]) {
 	FILE *mat, *resultFile;
 	clock_t t;
 	struct matrix m;
-	int i, j;
+	//int i, j;
 
 	mat = fopen(argv[1], "r");
 	fscanf(mat, "%d %d", &m.nrows, &m.ncols);
@@ -229,34 +223,22 @@ int main(int argc, char* argv[]) {
 	}
 	*/
 	
-	/* Memory allocation for the inverse matrix */
-	double **a_inv = (double**)malloc(m.nrows * sizeof(double*));
-	for (i = 0; i < m.ncols; i++) {
-        a_inv[i] = (double*)malloc(m.ncols * sizeof(double));
-    }
+	double *a_inv = (double*)malloc(m.nrows * m.ncols * sizeof(double));
+
     
 	t = clock();
 	computeInverse(m.mat, a_inv, m.nrows);
 	t = clock() - t;
 	
-	/* Linearization of the inverse matrix, necessary to use storeMatrix() */
-	double *inverse = (double*)malloc(m.nrows * m.ncols * sizeof(double));
-	for (i = 0; i < m.nrows; i++) {
-		for (j = 0; j < m.ncols; j++) {
-			inverse[i*m.ncols + j] = a_inv[i][j];
-		}
-	}
-	
 	resultFile = fopen("inverse.txt", "w");
-	storeMatrix(inverse, m.nrows, resultFile);
+	storeMatrix(a_inv, m.nrows, resultFile);
 
 	printf("\nElapsed time: %lf seconds\n", ((double)t) / CLOCKS_PER_SEC);
 
 	fclose(mat);
 	fclose(resultFile);
-	free(*a_inv);		
+	free(a_inv);		
 	free(m.mat);
-	free(inverse);
 
 	return 0;
 }
